9a73860393a61496ad3e28a1c15f6c726d9b2210
puneet <puneet098@hotmail.com>
2023-01-15 23:59:46 +0530

    udemy-springboot-h2

---
+++ udemy-springboot-h2/src/main/java/com/udemy/demo/UdemySpringbootH2Application.java
@@ -0,0 +1,13 @@
+package com.udemy.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemySpringbootH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemySpringbootH2Application.class, args);
+	}
+
+}
---
+++ udemy-springboot-h2/src/main/resources/schema.sql
@@ -0,0 +1,17 @@
+create table employee
+(
+	id int not null,
+	name varchar(255),
+	primary key (id)
+);
+insert into employee(id, name) values(1, 'Puneet');
+insert into employee(id, name) values(2, 'Vineet');
+
+create table course
+(
+	id bigint not null,
+	name varchar(255) not null,
+	author varchar(255) not null,
+	primary key(id)
+);
+insert into course(id, name, author) values(1, 'Learn AWS', 'Ranga');
